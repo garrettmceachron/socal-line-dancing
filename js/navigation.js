e9d345701d2fb11d6298f8cc6fcf94f98ac98a2e
garrettmceachron <garrettmceachron@gmail.com>
2026-07-21 03:46:11 -0700

    Remove .html from links

--- js/navigation.js
+++ js/navigation.js
@@ -11,10 +11,10 @@ if (currentLink.includes('/articles/')) {
 //   
 //}	
 
-document.getElementById("homeText").innerHTML = '<a class="dropdown-item" id="homeText" href="' + hrefDirectory + 'index.html"><img id="homeImage" src="' + srcDirectory + 'images/Logo.png" class="homeIcon">&nbsp&nbsp&nbsp&nbsp Home</a></img>';
-document.getElementById("locationsMapText").innerHTML = '<a class="dropdown-item" href="' + hrefDirectory + 'linedancelocations.html"><img src="' + srcDirectory + 'images/SaloonDoor.png" class="homeIcon">&nbsp&nbsp&nbsp&nbsp Locations Map</a></img>';
-document.getElementById("locationsInfoText").innerHTML = '<a class="dropdown-item" href="' + hrefDirectory + 'calendar.html"><img src="' + srcDirectory + 'images/WantedPosterLargerWanted.png" class="homeIcon">&nbsp&nbsp&nbsp&nbsp Calendar</a>';
-document.getElementById("guidesText").innerHTML = '<a class="dropdown-item" href="' + hrefDirectory + 'guides.html"><img src="' + srcDirectory + 'images/WesternStar.png" class="homeIcon">&nbsp&nbsp&nbsp&nbsp Guides</a>';
-document.getElementById("articlesText").innerHTML = '<a class="dropdown-item" href="' + hrefDirectory + 'articles.html"><img src="' + srcDirectory + 'images/HorseshoeMerged.png" class="homeIcon">&nbsp&nbsp&nbsp&nbsp Articles</a>';
-document.getElementById("aboutText").innerHTML = '<a class="dropdown-item" href="' + hrefDirectory + 'about.html"><img src="' + srcDirectory + 'images/CowboyHat.png" class="homeIcon">&nbsp&nbsp&nbsp&nbsp About</a>';
+document.getElementById("homeText").innerHTML = '<a class="dropdown-item" id="homeText" href="' + hrefDirectory + 'index"><img id="homeImage" src="' + srcDirectory + 'images/Logo.png" class="homeIcon">&nbsp&nbsp&nbsp&nbsp Home</a></img>';
+document.getElementById("locationsMapText").innerHTML = '<a class="dropdown-item" href="' + hrefDirectory + 'linedancelocations"><img src="' + srcDirectory + 'images/SaloonDoor.png" class="homeIcon">&nbsp&nbsp&nbsp&nbsp Locations Map</a></img>';
+document.getElementById("locationsInfoText").innerHTML = '<a class="dropdown-item" href="' + hrefDirectory + 'calendar"><img src="' + srcDirectory + 'images/WantedPosterLargerWanted.png" class="homeIcon">&nbsp&nbsp&nbsp&nbsp Calendar</a>';
+document.getElementById("guidesText").innerHTML = '<a class="dropdown-item" href="' + hrefDirectory + 'guides"><img src="' + srcDirectory + 'images/WesternStar.png" class="homeIcon">&nbsp&nbsp&nbsp&nbsp Guides</a>';
+document.getElementById("articlesText").innerHTML = '<a class="dropdown-item" href="' + hrefDirectory + 'articles"><img src="' + srcDirectory + 'images/HorseshoeMerged.png" class="homeIcon">&nbsp&nbsp&nbsp&nbsp Articles</a>';
+document.getElementById("aboutText").innerHTML = '<a class="dropdown-item" href="' + hrefDirectory + 'about"><img src="' + srcDirectory + 'images/CowboyHat.png" class="homeIcon">&nbsp&nbsp&nbsp&nbsp About</a>';
 
